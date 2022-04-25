e63c2a1b6197d75a11eb45a2c068052c4ec704c5
Matthew Wiser <matt.wiser@icloud.com>
2022-04-25 18:54:31 -0500

    created initital query statements for CRUD functionality

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -6,7 +7,48 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'wehttam1',
+        database: 'election'
+    }, 
+ console.log("Connected to election database")
+)
 
+// db.query(`SELECT * FROM CANDIDATES`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// // GET A SINGLE CANDIDATE
+// db.query(`SELECT * FROM  candidates WHERE id = 1`, (err, row) =>{
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+// // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+//   // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+// VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+// if (err) {
+// console.log(err);
+// }
+// console.log(result);
+// });
 
 //Default response for any other request
 app.use((req, res) => {
